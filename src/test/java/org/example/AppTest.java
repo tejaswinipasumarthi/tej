cbc264d98d8fb9c6dd82b974ba502665f3875fb3
Tejaswini <tejupasumarthi07@gmail.com>
2025-06-06 17:05:05 +0530

    pushing to git

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,9 @@
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals("HELLO", "HELLO");
+    }
+}
